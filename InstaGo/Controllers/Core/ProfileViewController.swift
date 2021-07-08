ccc3a630d2689dc633c24a37d67937e5a4410d61
phonglecanh <phonglecanh@gmail.com>
2021-07-08 15:23:57 +0700

    Signing Out

--- InstaGo/Controllers/Core/ProfileViewController.swift
+++ InstaGo/Controllers/Core/ProfileViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    private let user: User
+    private var isCurrentUser: Bool {
+        return user.username.lowercased() == UserDefaults.standard.string(forKey: "username")?.lowercased() ?? ""
+    }
+    
+    init(user: User) {
+        self.user = user
+        super.init(nibName: nil, bundle: nil)
+    }
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        title = "Profile"
+        title = user.username.lowercased()
         view.backgroundColor = .systemBackground
+        configure()
+    }
+    
+   
+     private func configure() {
+        if isCurrentUser {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(
+                image: UIImage(systemName: "gear"), style: .done, target: self, action: #selector(didTapSettings)
+            )
+        }
+    }
+    @objc func didTapSettings() {
+        let vc = SettingsViewController()
+        
+        present(UINavigationController(rootViewController: vc), animated: true)
     }
 
 }
