7592af27812ec0f7c7f29ba9ce74526949004a1f
phonglecanh <phonglecanh@gmail.com>
2021-07-11 11:44:37 +0700

    Taking Photos

--- InstaGo/Controllers/Core/CameraViewController.swift
+++ InstaGo/Controllers/Core/CameraViewController.swift
@@ -6,15 +6,137 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
 
+    private var output = AVCapturePhotoOutput()
+    private var captureSession: AVCaptureSession?
+    private let previewLayer =  AVCaptureVideoPreviewLayer()
+    private let cameraView = UIView()
+    
+    private let shutterButton: UIButton = {
+       let button = UIButton()
+        button.layer.masksToBounds = true
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.label.cgColor
+        button.backgroundColor = nil
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Take Photo"
+        view.backgroundColor = .secondarySystemBackground
+        view.addSubview(cameraView)
+        view.addSubview(shutterButton)
+        setUpNavBar()
+        checkCameraPermission()
+        shutterButton.addTarget(self, action: #selector(didTapTakePhoto), for: .touchUpInside)
+    }
 
-        title = "Camera"
-        view.backgroundColor = .systemBackground
-        
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        tabBarController?.tabBar.isHidden = true
+        if let session = captureSession, !session.isRunning {
+            session.startRunning()
+        }
     }
     
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        captureSession?.stopRunning()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        cameraView.frame = view.bounds
+        previewLayer.frame = CGRect(x: 0,
+                             y: view.safeAreaInsets.top,
+                             width: view.width,
+                             height: view.width
+        )
+        let buttonSize: CGFloat = view.width/5
+        shutterButton.frame = CGRect(x: (view.width-buttonSize)/2,
+                                     y: view.safeAreaInsets.top + view.width+100,
+                                     width: buttonSize,
+                                     height: buttonSize)
+        shutterButton.layer.cornerRadius = buttonSize/2
+    }
+    
+    @objc func didTapTakePhoto() {
+        output.capturePhoto(with: AVCapturePhotoSettings(), delegate: self)
+    }
+    
+    private func checkCameraPermission() {
+        switch AVCaptureDevice.authorizationStatus(for: AVMediaType.video) {
+        case .notDetermined:
+            // request
+            AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
+                guard granted else {
+                    return
+                }
+                DispatchQueue.main.async {
+                    self?.setUpCamera()
+                }
+            }
+        case .authorized:
+            setUpCamera()
+        case .restricted, .denied:
+            break
+        @unknown default:
+            break
+        }
+    }
+
+    private func setUpCamera() {
+        let captureSession = AVCaptureSession()
+        if let device = AVCaptureDevice.default(for: .video) {
+            do {
+                let input = try AVCaptureDeviceInput(device: device)
+                if captureSession.canAddInput(input) {
+                    captureSession.addInput(input)
+                }
+            } catch {
+                print(error)
+            }
+
+            if captureSession.canAddOutput(output) {
+                captureSession.addOutput(output)
+            }
+            // Layer
+           
+            previewLayer.session = captureSession
+            previewLayer.videoGravity = .resizeAspectFill
+            cameraView.layer.addSublayer(previewLayer)
+            captureSession.startRunning()
+        }
+    }
+
+    @objc func didTapClose() {
+        tabBarController?.selectedIndex = 0
+        tabBarController?.tabBar.isHidden = false
+    }
+
+    private func setUpNavBar() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .close,
+            target: self,
+            action: #selector(didTapClose))
+    }
 }
+
+extension CameraViewController: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard let data = photo.fileDataRepresentation(),
+              let image = UIImage(data: data) else {
+            return
+        }
+        captureSession?.stopRunning()
+        let vc = PostEditViewController(image: image)
+        if #available(iOS 14.0, *) {
+            vc.navigationItem.backButtonDisplayMode = .minimal
+        }
+        navigationController?.pushViewController(vc, animated: true)
+        }
+    }
